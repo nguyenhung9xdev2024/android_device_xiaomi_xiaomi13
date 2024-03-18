9044474184c71f1e055133d643265a76cbe6ec50
nguyenhung9xdev2024 <nguyenhung9xdev2024@gmail.com>
2024-03-18 08:45:27 +0700

    xiaomi13: Build telephony packages and disable rild

--- device.mk
+++ device.mk
@@ -356,6 +356,19 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.stepcounter.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepcounter.xml \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepdetector.xml
 
+# Telephony
+PRODUCT_PACKAGES += \
+    ims-ext-common \
+    ims_ext_common.xml \
+    qti-telephony-hidl-wrapper \
+    qti_telephony_hidl_wrapper.xml \
+    qti-telephony-utils \
+    qti_telephony_utils.xml \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+    
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0 \
