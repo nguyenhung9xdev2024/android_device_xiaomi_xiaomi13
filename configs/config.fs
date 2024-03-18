420ce57db2078a53d35565f0a4d96abf0b942343
nguyenhung9xdev2024 <nguyenhung9xdev2024@gmail.com>
2024-03-18 17:08:49 +0700

    xiaomi13: update configfs

--- configs/config.fs
+++ configs/config.fs
@@ -31,6 +31,30 @@ value:2910
 [AID_VENDOR_NXP_WEAVER]
 value:2911
 
+[AID_VENDOR_SSGTZD]
+value:2912
+
+[AID_VENDOR_THALES_STRONGBOX]
+value:2913
+
+[vendor/bin/hw/android.hardware.security.keymint-service.strongbox-thales]
+mode: 0755
+user: AID_VENDOR_THALES_STRONGBOX
+group: AID_SYSTEM
+caps: SYS_ADMIN SYS_NICE
+
+[vendor/bin/hw/android.hardware.security.keymint-service.strongbox]
+mode: 0755
+user: AID_VENDOR_NXP_STRONGBOX
+group: AID_SYSTEM
+caps: SYS_ADMIN SYS_NICE
+
+[vendor/bin/hw/android.hardware.weaver@1.0-service]
+mode: 0755
+user: AID_VENDOR_NXP_WEAVER
+group: AID_SYSTEM
+caps: SYS_ADMIN SYS_NICE
+
 [vendor/bin/wcnss_filter]
 mode: 0755
 user: AID_BLUETOOTH
@@ -120,18 +144,6 @@ mode: 0755
 user: AID_RADIO
 group: AID_RADIO
 caps: NET_BIND_SERVICE
-
-[vendor/bin/hw/android.hardware.security.keymint-service.strongbox]
-mode: 0755
-user: AID_VENDOR_NXP_STRONGBOX
-group: AID_SYSTEM
-caps: SYS_ADMIN SYS_NICE
-
-[vendor/bin/hw/android.hardware.weaver@1.0-service]
-mode: 0755
-user: AID_VENDOR_NXP_WEAVER
-group: AID_SYSTEM
-caps: SYS_ADMIN SYS_NICE
 
 [vendor/bin/ims_rtp_daemon]
 mode: 0755
@@ -245,10 +257,4 @@ caps: 0
 mode: 0771
 user: AID_MEDIA
 group: AID_MEDIA
-caps: 0
-
-[odm/overlayfs/*/*/bin/*]
-mode: 0755
-user: AID_ROOT
-group: AID_SHELL
 caps: 0
